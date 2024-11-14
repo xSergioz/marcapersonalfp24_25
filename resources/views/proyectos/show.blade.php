e16e7781b76efaad9d2ef4d40bec6076f5c7871c
Jose Maria <josemariagarrehermosilla5@gmail.com>
2024-11-14 09:44:56 +0100

    Solución ejercicio vista

---
+++ resources/views/proyectos/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('content')
+
+    Vista detalle proyecto {{$id}}
+
+@stop
+
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('login', function() {
-    return 'Login usuario';
+    return view('auth.login');
 });
 
 Route::get('logout', function() {
@@ -15,19 +15,19 @@ Route::get('logout', function() {
 });
 
 Route::get('proyectos', function() {
-    return 'Listado proyectos';
+    return view('proyectos.index');
 });
 
 Route::get('proyectos/show/{id}', function($id) {
-    return 'Vista detalle proyecto ' . $id;
+    return view('proyectos.show', array('id'=>$id));
 })->where('id', '[0-9]+');
 
 Route::get('proyectos/create', function() {
-    return 'Añadir proyecto';
+    return view('proyectos.create');
 });
 
 Route::get('proyectos/edit/{id}', function($id) {
-    return 'Modificar proyecto ' . $id;
+    return view('proyectos.edit', array('id'=>$id));
 })->where('id', '[0-9]+');
 
 Route::get('perfil/{id?}', function($id = null) {
