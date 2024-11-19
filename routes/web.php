2faf2c2119589dec12003e09d6a6d602180df21f
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-11-19 09:30:23 +0100

    Solución del ejercicio hasta el index de proyectos

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'getHome']);
 
 Route::get('login', function() {
     return view('auth.login');
@@ -14,13 +14,10 @@ Route::get('logout', function() {
     return 'Logout usuario';
 });
 
-Route::get('proyectos', function() {
-    return view('proyectos.index');
-});
+Route::get('proyectos', [ProyectosController::class, 'getIndex']);
 
-Route::get('proyectos/show/{id}', function($id) {
-    return view('proyectos.show', array('id'=>$id));
-})->where('id', '[0-9]+');
+Route::get('proyectos/show/{id}', [ProyectosController::class, 'getShow'])
+->where('id', '[0-9]+');
 
 Route::get('proyectos/create', function() {
     return view('proyectos.create');
