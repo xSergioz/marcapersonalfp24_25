1f94fc629ebabf5b933e408ce16576af22bcb5a0
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-11-20 09:49:06 +0100

    rutas distribuidas en ficheros distintos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProyectosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'getHome']);
@@ -14,15 +13,12 @@ Route::get('logout', function() {
     return 'Logout usuario';
 });
 
-Route::get('proyectos', [ProyectosController::class, 'getIndex']);
-
-Route::get('proyectos/show/{id}', [ProyectosController::class, 'getShow'])
-->where('id', '[0-9]+');
-
-Route::get('proyectos/create', [ProyectosController::class, 'getCreate']);
-
-Route::get('proyectos/edit/{id}', [ProyectosController::class, 'getEdit'])->where('id', '[0-9]+');
-
 Route::get('perfil/{id?}', function($id = null) {
     return $id ? 'Visualizar el currículo de '. $id : 'Visualizar el currículo propio';
 })->where('id', '[0-9]*');
+
+include __DIR__.'/actividades.php';
+include __DIR__.'/curriculos.php';
+include __DIR__.'/proyectos.php';
+include __DIR__.'/reconocimientos.php';
+include __DIR__.'/users.php';
