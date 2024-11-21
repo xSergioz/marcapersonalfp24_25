38fa627154de30b19445b604c2a9e8799acdb802
Pablo Palazon <pablocryystalz24@gmail.com>
2024-11-21 09:35:04 +0100

    Ejercicio UserController Resuelto

--- routes/users.php
+++ routes/users.php
@@ -1,1 +1,8 @@
 <?php
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('users', [UserController::class, 'getIndex']);
+Route::get('users/show/{id}', [UserController::class, 'getShow'])->where('id', '[0-9]*');
+Route::get('users/create', [UserController::class, 'getCreate']);
+Route::get('users/edit/{id}', [UserController::class, 'getEdit'])->where('id', '[0-9]*');
