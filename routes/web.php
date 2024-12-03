8c9de41c5f1e391b1636d484176e3bbc5e1f0fbd
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-12-03 09:53:24 +0100

    Columnas de Estudiantes y gestión de registros

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Models\Estudiante;
 use Illuminate\Support\Facades\Route;
 
 
@@ -18,7 +19,45 @@ Route::get('perfil/{id?}', function($id = null) {
     return $id ? 'Visualizar el currículo de '. $id : 'Visualizar el currículo propio';
 })->where('id', '[0-9]*');
 
+Route::get('pruebaDB/{votos?}', function ($votos = null) {
+    $count = Estudiante::where('votos', '>', 100)->count();
+    $max = Estudiante::max('votos');
+    $min = Estudiante::min('votos');
+    $media = Estudiante::avg('votos');
+    $total = Estudiante::sum('votos');
+    $html = '<ul>';
+    $html .= '<li>Estudiantes con más de 100 votos: ' . $count . '</li>';
+    $html .= '<li>Máximo número de votos: ' . $max . '</li>';
+    $html .= '<li>Mínimo número de votos: ' . $min . '</li>';
+    $html .= '<li>Media de votos: ' . $media . '</li>';
+    $html .= '<li>Total de votos: ' . $total . '</li>';
+    $html .= "</ul>\n<ul>";
 
+    $estudiantes = Estudiante::where('votos', '>', $votos)->take(5)->get();
+     foreach ($estudiantes as $est) {
+        $html .= '<li>' . $est->nombre . '</li>';
+    };
+
+    $count = Estudiante::where('votos', '>', 100)->count();
+    $html .= '</ul>';
+    $html .= 'Antes: ' . $count . '<br />';
+
+    // $estudiante = new Estudiante;
+    $id = $votos ?? 1;
+    $estudiante = Estudiante::findOrFail($id);
+    $estudiante->nombre = 'Juan';
+    $estudiante->apellidos = 'Martínez';
+    $estudiante->direccion = 'Dirección de Juan';
+    $estudiante->votos = 130;
+    $estudiante->confirmado = true;
+    $estudiante->ciclo = 'DAW';
+    $estudiante->save();
+
+    $count = Estudiante::where('votos', '>', 100)->count();
+    $html .= 'Después: ' . $count . '<br />';
+    return $html;
+    // return $estudiante->nombre;
+});
 
 
 
