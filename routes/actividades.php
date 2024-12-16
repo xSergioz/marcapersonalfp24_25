8a01d9428fb8f533a8f1b29bc77aad9085d9e8d0
Sergio Vera <1698224@alu.murciaeduca.es>
2024-12-16 08:17:19 +0100

    Arreglando fallos

--- routes/actividades.php
+++ routes/actividades.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ActividadController;
+use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'actividades'], function(){
     Route::get('/', [ActividadController::class, 'getIndex']);
