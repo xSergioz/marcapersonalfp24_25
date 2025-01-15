3aedf8fbd80dd35aa29155220a1e563930310348
Sergio Vera <1698224@alu.murciaeduca.es>
2025-01-15 09:10:35 +0100

    Controlador de recursos de curriculos

--- app/Models/Curriculo.php
+++ app/Models/Curriculo.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curriculo extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'user_id',
+        'video_curriculum',
+        'texto_curriculum'
+    ];
 }
