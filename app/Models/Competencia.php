869c03895717239546cb4ad52b5a4ba6bb04d5e6
Daniel Soto Ortiz <daniel.soto.ortiz@gmail.com>
2025-01-27 09:54:49 +0100

    Ejercicio

---
+++ app/Models/Competencia.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Competencia extends Model
+{
+    use HasFactory;
+
+    protected $table = 'competencias';
+
+    protected $fillable = [
+        'id',
+        'nombre',
+        'color'
+
+    ];
+}
---
+++ database/seeders/CompetenciasTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Competencia;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CompetenciasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Competencia::truncate();
+
+        if(Competencia::count() == 0) {
+            if(config('app.env') ==='local'){
+                //Competencia::factory(10)->create();
+                Competencia::factory()->create([
+                'nombre' => 'Prueba Nombre',
+                'color' => 'colorPrueba',
+                'actividas_id' => 1
+                ]);
+            }
+        }
+    }
+}
