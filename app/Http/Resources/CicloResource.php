ca06a7e049d8eba7bf0657c78bbdba08acb2a67b
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-02-05 17:57:14 +0100

    relación de belongsTo de ciclo con familias (#363)

--- app/Http/Resources/CicloResource.php
+++ app/Http/Resources/CicloResource.php
@@ -14,6 +14,9 @@ class CicloResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return array_merge(parent::toArray($request), [
+            'familia_profesional' => $this->familiaProfesional,
+        ]);
     }
 }
--- app/Models/Ciclo.php
+++ app/Models/Ciclo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Ciclo extends Model
 {
@@ -19,4 +20,9 @@ class Ciclo extends Model
     ];
 
     public static $filterColumns = ['codCiclo', 'codFamilia', 'grado', 'nombre'];
+
+    public function familiaProfesional() : BelongsTo
+    {
+        return $this->belongsTo(FamiliaProfesional::class, 'familia_id');
+    }
 }
