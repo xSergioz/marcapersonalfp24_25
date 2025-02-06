0addea75e2660d60fcdbcfcb4a2cde646b9fae9c
Gorka <659304@alu.murciaeduca.es>
2025-02-06 08:53:14 +0100

    Relaciones de ciclo con user

--- app/Http/Resources/CicloResource.php
+++ app/Http/Resources/CicloResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\FamiliaProfesional;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,22 @@ class CicloResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $padre=parent::toArray($request);
+        $familiaProfesional = $this->FamiliaProfesional;
+        $familia=
+        [
+            "familia_id"=> [
+              "id"=>$familiaProfesional->id,
+              "codigo"=>$familiaProfesional->codigo ,
+              "nombre"=>$familiaProfesional->nombre
+            ]
+        ];
+        unset($padre['created_at']);
+        unset($padre['updated_at']);
+
+        return array_merge(
+            $padre,
+            $familia
+        );
     }
 }
