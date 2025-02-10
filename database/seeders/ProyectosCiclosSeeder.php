70728c3dc263538b6cf7eb7512a07ce902f03949
Adrián Martínez <4281885@alu.murciaeduca.es>
2025-02-10 09:20:42 +0100

    correcciones proyectosCiclos

--- database/seeders/ProyectosCiclosSeeder.php
+++ database/seeders/ProyectosCiclosSeeder.php
@@ -19,10 +19,11 @@ class ProyectosCiclosSeeder extends Seeder
         $proyectos = DB::table('proyectos')->get();
 
         foreach ($ciclos as $ciclo) {
-            // Asignar proyectos aleatorios a cada ciclo
-            $proyectosAsignados = $proyectos->random(rand(1, $proyectos->count()));
 
-            foreach ($proyectosAsignados as $proyecto) {
+            $registros = rand(0, 2);
+
+            for ($i = 0; $i < $registros; $i++) {
+                $proyecto = $proyectos->random();
                 DB::table('proyectos_ciclos')->insert([
                     'ciclo_id' => $ciclo->id,
                     'proyecto_id' => $proyecto->id,
