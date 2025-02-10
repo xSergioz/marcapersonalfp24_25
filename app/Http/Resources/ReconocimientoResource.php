e759a641ffb3a8341aa25b0ee754ce938ab458bc
David Paredes <1918862@alu.murciaeduca.es>
2025-02-10 09:04:48 +0100

    Solucionado el ReconocimientosResource y coomprobado en el swagger

--- app/Http/Resources/ReconocimientoResource.php
+++ app/Http/Resources/ReconocimientoResource.php
@@ -14,6 +14,10 @@ class ReconocimientoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+      return array_merge(parent::toArray($request), [
+        'actividad' => $this->actividad
+    ]);
+
+
     }
 }
